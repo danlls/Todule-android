6630eb485fd01a239a087b5324cb2a69c2062b63
danieL <daniellee468@gmail.com>
2017-08-21 14:37:58 +0800

    Replace editext with TextInputLayout

--- app/src/main/java/com/example/daniel/todule_android/activities/ToduleAddFragment.java
+++ app/src/main/java/com/example/daniel/todule_android/activities/ToduleAddFragment.java
@@ -3,9 +3,11 @@ package com.example.daniel.todule_android.activities;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +29,6 @@ import java.util.Calendar;
 public class ToduleAddFragment extends Fragment implements View.OnClickListener{
     Calendar myCalendar = Calendar.getInstance();
 
-
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
         ((MainActivity)getActivity()).fabVisibility(false);
@@ -39,13 +40,71 @@ public class ToduleAddFragment extends Fragment implements View.OnClickListener{
         Button cancelButton = (Button) view.findViewById(R.id.cancel_button);
         cancelButton.setOnClickListener(this);
 
-        EditText dateEdit = (EditText) view.findViewById(R.id.edit_date);
-        dateEdit.setOnClickListener(this);
+        final EditText dateEdit = view.findViewById(R.id.edit_date);
+        final EditText timeEdit = view.findViewById(R.id.edit_time);
+        dateEdit.setInputType(InputType.TYPE_NULL);
+        timeEdit.setInputType(InputType.TYPE_NULL);
+
+        dateEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(final View view, boolean b) {
+                if (b) {
+                    ((MainActivity) getActivity()).hideSoftKeyboard();
+                    DatePickerDialog datePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                            myCalendar.set(Calendar.YEAR, i);
+                            myCalendar.set(Calendar.MONTH, i1);
+                            myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+                            DateFormat df = DateFormat.getDateInstance();
+                            EditText ed = (EditText) view;
+                            ed.setText(df.format(myCalendar.getTime()));
+                        }
+                    }, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH));
+                    datePicker.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                    datePicker.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialogInterface) {
+                            dateEdit.clearFocus();
+                        }
+                    });
+                    datePicker.show();
+                }
+            }
+        });
+        timeEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(final View view, boolean b) {
+                if (b) {
+                    ((MainActivity) getActivity()).hideSoftKeyboard();
+                    TimePickerDialog timePicker = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                        @Override
+                        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                            myCalendar.set(Calendar.HOUR_OF_DAY, i);
+                            myCalendar.set(Calendar.MINUTE, i1);
+                            DateFormat dft = DateFormat.getTimeInstance();
+                            EditText ed = (EditText) view;
+                            ed.setText(dft.format(myCalendar.getTime()));
+                        }
+                    }, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), true);
+                    timePicker.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @Override
+                        public void onDismiss(DialogInterface dialogInterface) {
+                            timeEdit.clearFocus();
+                        }
+                    });
+                    timePicker.show();
+                }
+            }
+        });
+
+        // Set default
+        myCalendar.set(Calendar.SECOND, 0);
         DateFormat df = DateFormat.getDateInstance();
         dateEdit.setText(df.format(myCalendar.getTime()));
-        EditText timeEdit = (EditText) view.findViewById(R.id.edit_time);
-        timeEdit.setOnClickListener(this);
-        timeEdit.setText(String.format("%02d", Calendar.HOUR_OF_DAY) + ": " + String.format("%02d", Calendar.MINUTE));
+        DateFormat df2 = DateFormat.getTimeInstance();
+        timeEdit.setText(df2.format(myCalendar.getTime()));
 
         return view;
     }
@@ -60,34 +119,6 @@ public class ToduleAddFragment extends Fragment implements View.OnClickListener{
                 break;
             case R.id.cancel_button:
                 getActivity().onBackPressed();
-                break;
-            case R.id.edit_date:
-                final EditText editDate = (EditText) view;
-                DatePickerDialog datePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-                        myCalendar.set(Calendar.YEAR, i);
-                        myCalendar.set(Calendar.MONTH, i1);
-                        myCalendar.set(Calendar.DAY_OF_MONTH, i2);
-                        DateFormat df = DateFormat.getDateInstance();
-                        editDate.setText(df.format(myCalendar.getTime()));
-                    }
-                }, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH));
-                datePicker.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
-                datePicker.show();
-                break;
-            case R.id.edit_time:
-                final EditText editTime = (EditText) view;
-                new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
-                        myCalendar.set(Calendar.HOUR_OF_DAY, i);
-                        myCalendar.set(Calendar.MINUTE, i1);
-                        editTime.setText(String.format("%02d", i) + ":" + String.format("%02d", i1));
-                    }
-                }, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), true)
-                        .show();
                 break;
         }
     }
