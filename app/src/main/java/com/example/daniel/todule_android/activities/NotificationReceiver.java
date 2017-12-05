7b59c4f849d8a35fa027ca445158d63249437312
danieL <daniellee468@gmail.com>
2017-12-05 11:42:30 +0800

    Add 'mark as done' action in reminder notification

--- app/src/main/java/com/example/daniel/todule_android/activities/NotificationReceiver.java
+++ app/src/main/java/com/example/daniel/todule_android/activities/NotificationReceiver.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -79,6 +80,19 @@ public class NotificationReceiver extends BroadcastReceiver {
                                     PendingIntent.FLAG_CANCEL_CURRENT
                             );
 
+                    // Intent to mark todule as done in notification
+                    Intent markToduleDoneIntent = new Intent(context, getClass());
+                    markToduleDoneIntent.setAction("com.example.daniel.todule_android.TODULE_DONE");
+                    markToduleDoneIntent.putExtra("todule_id", entryId);
+
+                    PendingIntent markToduleDonePendingIntent =
+                            PendingIntent.getBroadcast(
+                                    context,
+                                    0,
+                                    markToduleDoneIntent,
+                                    PendingIntent.FLAG_CANCEL_CURRENT
+                            );
+
                     // Construct notification
                     NotificationCompat.Builder mBuilder =
                             new NotificationCompat.Builder(context, CHANNEL_ID)
@@ -90,6 +104,7 @@ public class NotificationReceiver extends BroadcastReceiver {
                                     .setAutoCancel(true);
 
                     mBuilder.setDefaults(Notification.DEFAULT_ALL);
+                    mBuilder.addAction(R.drawable.ic_done_white_18dp, "Mark as done", markToduleDonePendingIntent);
 
                     // Gets an instance of the NotificationManager service
                     NotificationManager mNotifyMgr =
@@ -101,6 +116,16 @@ public class NotificationReceiver extends BroadcastReceiver {
                 } else if(action.equals("com.example.daniel.todule_android.DELETE_REMINDER")) {
                     long toduleId = intent.getLongExtra("todule_id", -1L);
                     NotificationHelper.cancelReminder(context, toduleId);
+                } else if(action.equals("com.example.daniel.todule_android.TODULE_DONE")){
+                    long toduleId = intent.getLongExtra("todule_id", -1L);
+                    NotificationManager mNotifyMgr =
+                            (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+                    mNotifyMgr.cancel((int) toduleId);
+                    Uri toduleUri = ContentUris.withAppendedId(ToduleDBContract.TodoEntry.CONTENT_ID_URI_BASE, toduleId);
+                    ContentValues cv = new ContentValues();
+                    cv.put(ToduleDBContract.TodoEntry.COLUMN_NAME_TASK_DONE, ToduleDBContract.TodoEntry.TASK_COMPLETED);
+                    cv.put(ToduleDBContract.TodoEntry.COLUMN_NAME_COMPLETED_DATE, System.currentTimeMillis());
+                    context.getContentResolver().update(toduleUri, cv , null, null);
                 }
             }
 
